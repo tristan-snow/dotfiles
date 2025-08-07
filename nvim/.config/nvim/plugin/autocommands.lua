5f24d8b5e874a2641e545344e0f60146d5b5d671
Tristan Snow <tristansnowcobb@gmail.com>
2025-08-07 15:38:28 -0400

    Added highlight on yank

--- nvim/.config/nvim/plugin/autocommands.lua
+++ nvim/.config/nvim/plugin/autocommands.lua
@@ -9,4 +9,10 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
-
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
